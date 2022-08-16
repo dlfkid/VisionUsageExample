9b03969717ff6b6fd6af6820542036c78117d79f
ravendeng <ravendeng@tencent.com>
2022-08-16 19:27:02 +0800

    增加人脸追踪的入口

---
+++ VisionUsageExample/FaceTrackViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FaceTrackViewController.swift
+//  VisionUsageExample
+//
+//  Created by ravendeng on 2022/8/16.
+//
+
+import UIKit
+
+class FaceTrackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "人脸追踪"
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- VisionUsageExample/ViewController.swift
+++ VisionUsageExample/ViewController.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 import PinLayout
+import SwiftUI
 
 class ViewController: UIViewController {
+    
+    let faceTrackButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Vision使用Example"
-        // Do any additional setup after loading the view.
+        
+        faceTrackButton.setTitle("人脸追踪", for: .normal);
+        faceTrackButton.addTarget(self, action: #selector(faceTrackButtonDidTappedAction(sender:)), for: .touchUpInside)
+        view.addSubview(faceTrackButton)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        faceTrackButton.pin.top(view.pin.safeArea).margin(10).height(44).width(100).hCenter()
+    }
+    
+    @objc private func faceTrackButtonDidTappedAction(sender: UIButton) {
+        let faceTrackViewController = FaceTrackViewController()
+        navigationController?.pushViewController(faceTrackViewController, animated: true)
+    }
 }
 
