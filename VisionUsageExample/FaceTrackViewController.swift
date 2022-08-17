04b9a489bccd71dbd65223299c1a1446b2e92b46
ravendeng <ravendeng@tencent.com>
2022-08-17 16:54:57 +0800

    摄像头调用功能实现

--- VisionUsageExample/FaceTrackViewController.swift
+++ VisionUsageExample/FaceTrackViewController.swift
@@ -6,25 +6,78 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
 class FaceTrackViewController: UIViewController {
+    
+    let cameraView = UIView();
+    
+    let captureButton:UIButton = {
+        let captureButton = UIButton(type: .system)
+        captureButton.setTitle("开始捕捉", for: .normal)
+        captureButton.setTitle("停止捕捉", for: .selected)
+        return captureButton
+    }()
+    
+    private let visionSequenceHandler = VNSequenceRequestHandler()
+    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+        else { return session }
+        session.addInput(input)
+        return session
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "人脸追踪"
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        view.addSubview(cameraView)
+        view.addSubview(captureButton)
+        captureButton.addTarget(self, action: #selector(captureButtonDidTappedAction(sender:)), for: .touchUpInside)
+        cameraView.layer.addSublayer(cameraLayer)
+        // register to receive buffers from the camera
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
+        self.captureSession.addOutput(videoOutput)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraView.pin.all(view.pin.safeArea)
+        captureButton.pin.bottom(100).width(120).height(44).hCenter(0)
+    }
+}
+
+extension FaceTrackViewController {
+    @objc private func captureButtonDidTappedAction(sender:UIButton) {
+        sender.isSelected = !sender.isSelected
+        if (sender.isSelected) {
+            DispatchQueue.global().async {
+                // begin the session
+                self.captureSession.startRunning()
+            }
+        } else {
+            DispatchQueue.global().async {
+                // end the session
+                self.captureSession.stopRunning()
+            }
+        }
+    }
+}
+
+extension FaceTrackViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
     }
-    */
-
 }
